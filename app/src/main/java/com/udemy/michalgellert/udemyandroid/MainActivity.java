710dd3d989df92c5795e22f56c104e5f9d2abaa3
michal.gellert <michalgellert@gmail.com>
2017-10-17 23:29:30 +0200

    part 5 - main layout

--- app/src/main/java/com/udemy/michalgellert/udemyandroid/MainActivity.java
+++ app/src/main/java/com/udemy/michalgellert/udemyandroid/MainActivity.java
@@ -33,14 +33,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @OnClick(R.id.shoppingListButton)
